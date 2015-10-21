bcf267f3b224b89b52a3a0443d6ed650a88f0d6f
pius <piusvd@gmail.com>
2015-10-21 16:18:10 +0200

    implement stochastic gradient descent

--- SVM/handout/code/mapper.py
+++ SVM/handout/code/mapper.py
@@ -7,14 +7,36 @@ import numpy as np
 DIMENSION = 400  # Dimension of the original data.
 CLASSES = (-1, +1)   # The classes that we are trying to predict.
 
+LAMBDA = 1.0
+
 def transform(x_original):
     return x_original
 
 if __name__ == "__main__":
+    inv_sqrt_lambda = 1.0 / np.sqrt(LAMBDA)
+    w = np.zeros(DIMENSION)
+    #w = np.random.random(DIMENSION)
+    nu = 1.0
+    nb_iter = 1
     for line in sys.stdin:
         line = line.strip()
         (label, x_string) = line.split(" ", 1)
         label = int(label)
         x_original = np.fromstring(x_string, sep=' ')
         x = transform(x_original)  # Use our features.
+
+        score = label * x.dot(w)
+
+        nu = 1.0 / np.sqrt(nb_iter)
+
+        if score < 1.0:
+            unprojected = w - nu * label * x
+            inv_norm = 1.0 / np.linalg.norm(unprojected)
+            w = min(1.0, inv_sqrt_lambda * inv_norm) * unprojected
+
+        nb_iter = nb_iter + 1
+
+    # use dummy key to ensure that we reduce over all mapper outputs
+    str_repr = ' '.join(map(str, w))
+    print "{key}\t{val}".format(key=1, val=str_repr)
 
--- SVM/handout/code/reducer.py
+++ SVM/handout/code/reducer.py
@@ -5,7 +5,21 @@ import logging
 import sys
 import numpy as np
 
+from mapper import DIMENSION
+
 if __name__ == "__main__":
+    count = 0
+    w_acc = np.zeros(DIMENSION)
     for line in sys.stdin:
         line = line.strip()
+
+        key, value = line.split('\t')
+
+        w_acc = w_acc + np.fromstring(value, sep=' ')
+        count = count + 1
+
+    w_final = (1.0 / count) * w_acc
+    str_repr = ' '.join(map(str, w_final))
+
+    print str_repr
     
