ff5ab6ad41053b978284bedbefd7004af79790d7
Floran Gmehlin <florangmehlin@Florans-MacBook-Air.local>
2015-11-22 17:52:43 +0100

    First version of mapper with coresets done

--- KMEANS/code/mapper.py
+++ KMEANS/code/mapper.py
@@ -3,7 +3,66 @@
 
 import sys
 import numpy as np
+import random
+from scipy.spatial.distance import cdist
 
-for line in sys.stdin:
-    line = line.strip()
+def uniq(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
+
+if __name__ == "__main__":
+    d = 500
+    k = 100
+    epsilon = 0.999 # How to choose epsilon ?
+    
+    D = np.zeros(shape=(0,d))
+    
+
+    t = 0.3
+
+    np.random.seed(seed=0)
+
+    for line in sys.stdin:
+        line = line.strip()
+        # Create sampling distribution
+        if random.random < t:
+            sample = np.fromstring(line, sep=" ")
+            D = np.vstack( (D, sample) )
+
+
+
+    # Coresets via Adaptive Sampling
+    B = []
+    D_prime = D
+    
+    while(len(D_prime) != 0):
+        numSamples = 10 * d * k * np.log(1/epsilon)
+        sample_indices = np.random.randint(0, len(D_prime), size=numSamples) # for S
+       
+        S = [D_prime[i] for i in sample_indices]
+
+        # Get nearest points from S to D'
+        dist = cdist(S, D_prime), 'euclidean')
+        # Zip distances and sample index
+        distances_tuple = [ (dist[i][j], i) for i in xrange(len(dist)) for j in xrange(len(dist[0])) ]
+        distances_tuple = sorted(set(distances_tuple), key=lambda x: x[0])
+        # Remove 0 distances
+        distances_tuple = [t for t in distances_tuple if t[0] != 0.0]
+        # Remove nearest points
+        indices_to_remove = uniq(map(lambda e: e[1], distances_tuple))
+        D_prime = np.asarray([ D_prime[t] for t in indices_to_remove[:(len(D_prime)/2)] ])
+        B+=S
+
+    # Remove enventual duplicates
+    B = np.unique(map(lambda a: str(list(a.flat)), B))
+
+    emit(1, map(str, B))
+        
+
+
+
+
+
     
