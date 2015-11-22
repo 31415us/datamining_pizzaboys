c1bd19714d7077c0ac6d421295622c0b47afaa93
Floran Gmehlin <florangmehlin@Florans-MacBook-Air.local>
2015-11-22 18:44:44 +0100

    mapper still buggy, but concept is here

--- KMEANS/code/mapper.py
+++ KMEANS/code/mapper.py
@@ -6,28 +6,37 @@ import numpy as np
 import random
 from scipy.spatial.distance import cdist
 
+
+d = 500
+k = 100
+epsilon = 0.999 # How to choose epsilon ?
+verbose = False
+
+
 def uniq(seq):
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
 
+def emit(key, value):
+    print('%s\t%s' % (key, value))
 
 if __name__ == "__main__":
-    d = 500
-    k = 100
-    epsilon = 0.999 # How to choose epsilon ?
-    
+    if len(sys.argv) > 1 and sys.argv[1] == '-v':
+        verbose = True
+
     D = np.zeros(shape=(0,d))
     
 
     t = 0.3
 
-    np.random.seed(seed=0)
+    np.random.seed(seed=42)
 
     for line in sys.stdin:
         line = line.strip()
         # Create sampling distribution
-        if random.random < t:
+
+        if random.random() < t:
             sample = np.fromstring(line, sep=" ")
             D = np.vstack( (D, sample) )
 
@@ -37,14 +46,14 @@ if __name__ == "__main__":
     B = []
     D_prime = D
     
-    while(len(D_prime) != 0):
-        numSamples = 10 * d * k * np.log(1/epsilon)
+    while len(D_prime) > 0:
+        numSamples = min(len(D_prime), 10 * d * k * np.log(1/epsilon))
         sample_indices = np.random.randint(0, len(D_prime), size=numSamples) # for S
        
         S = [D_prime[i] for i in sample_indices]
 
-        # Get nearest points from S to D'
-        dist = cdist(S, D_prime), 'euclidean')
+        # Compute distance from S to D'
+        dist = cdist(S, D_prime, 'euclidean')
         # Zip distances and sample index
         distances_tuple = [ (dist[i][j], i) for i in xrange(len(dist)) for j in xrange(len(dist[0])) ]
         distances_tuple = sorted(set(distances_tuple), key=lambda x: x[0])
@@ -52,6 +61,13 @@ if __name__ == "__main__":
         distances_tuple = [t for t in distances_tuple if t[0] != 0.0]
         # Remove nearest points
         indices_to_remove = uniq(map(lambda e: e[1], distances_tuple))
+        if verbose:
+            print '--------------------'
+            print 'Number of samples = '+ str(numSamples)
+            print 'Previous len of D_prime = '+ str(len(D_prime))
+            print 'New len of D_prime = '+ str(len(D_prime)/2)
+
+
         D_prime = np.asarray([ D_prime[t] for t in indices_to_remove[:(len(D_prime)/2)] ])
         B+=S
 
