557d446493435b95aa9873b470b283f9c2f421f9
pius <piusvd@gmail.com>
2015-11-28 15:37:48 +0100

    minibatch kmeans mapper with uniform subsampling of dataset

--- KMEANS/code/mapper.py
+++ KMEANS/code/mapper.py
@@ -4,14 +4,15 @@
 import sys
 import numpy as np
 import random
-from scipy.spatial.distance import cdist
+
+from sklearn.cluster import MiniBatchKMeans
 
 
 d = 500
 k = 100
-epsilon = 0.999 # How to choose epsilon ?
 verbose = False
 
+t = 0.1
 
 def uniq(seq):
     seen = set()
@@ -25,60 +26,20 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == '-v':
         verbose = True
 
-    D = np.zeros(shape=(0,d))
-    
-
-    t = 0.3
-
-    np.random.seed(seed=42)
+    X = np.zeros(shape=(0,d))
 
     for line in sys.stdin:
         line = line.strip()
         # Create sampling distribution
-
+        sample = np.fromstring(line, sep=" ")
         if random.random() < t:
-            sample = np.fromstring(line, sep=" ")
-            D = np.vstack( (D, sample) )
+            X = np.vstack( (X, sample) )
 
+    clusterer = MiniBatchKMeans(n_clusters=k, batch_size=1000, n_init=10)
 
+    clusterer.fit(X)
 
-    # Coresets via Adaptive Sampling
-    B = []
-    D_prime = D
-    
-    while len(D_prime) > 0:
-        numSamples = min(len(D_prime), 10 * d * k * np.log(1/epsilon))
-        sample_indices = np.random.randint(0, len(D_prime), size=numSamples) # for S
-       
-        S = [D_prime[i] for i in sample_indices]
+    for center in clusterer.cluster_centers_:
+        str_repr = ' '.join(map(str, center))
+        emit(1, str_repr)
 
-        # Compute distance from S to D'
-        dist = cdist(S, D_prime, 'euclidean')
-        # Zip distances and sample index
-        distances_tuple = [ (dist[i][j], i) for i in xrange(len(dist)) for j in xrange(len(dist[0])) ]
-        distances_tuple = sorted(set(distances_tuple), key=lambda x: x[0])
-        # Remove 0 distances
-        distances_tuple = [t for t in distances_tuple if t[0] != 0.0]
-        # Remove nearest points
-        indices_to_remove = uniq(map(lambda e: e[1], distances_tuple))
-        if verbose:
-            print '--------------------'
-            print 'Number of samples = '+ str(numSamples)
-            print 'Previous len of D_prime = '+ str(len(D_prime))
-            print 'New len of D_prime = '+ str(len(D_prime)/2)
-
-
-        D_prime = np.asarray([ D_prime[t] for t in indices_to_remove[:(len(D_prime)/2)] ])
-        B+=S
-
-    # Remove enventual duplicates
-    B = np.unique(map(lambda a: str(list(a.flat)), B))
-
-    emit(1, map(str, B))
-        
-
-
-
-
-
-    
