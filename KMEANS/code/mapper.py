6e987594aace53e24107bf57b575b89aca1c87cb
Floran Gmehlin <florangmehlin@public-docking-cx-1108.ethz.ch>
2015-11-28 16:30:27 +0100

    mapper unusable

--- KMEANS/code/mapper.py
+++ KMEANS/code/mapper.py
@@ -6,14 +6,16 @@ import numpy as np
 import random
 from scipy.spatial.distance import cdist
 from scipy.spatial.distance import euclidean
-from scipy.spatial import Voronoi
 from scipy.spatial import KDTree
+from math import ceil, log10
 
 
 d = 500
 k = 100
 epsilon = 0.999 # How to choose epsilon ?
+delta  = 0.15
 verbose = False
+
 
 
 def uniq(seq):
@@ -24,18 +26,20 @@ def uniq(seq):
 def emit(key, value):
     print('%s\t%s' % (key, value))
     
-def q(x, b, Db, sumDist):
-    sumDist = 
-    return (5/len(Db)+ (euclidean(x, b)**2)/(sumDist**2))
+def gamma(x, b, Db, sumDist): 
+    q = ceil((5/len(Db)+ (euclidean(x, b)**2)/(sumDist**2)))
+    return 1.0/q
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == '-v':
         verbose = True
 
-    D = np.zeros(shape=(0,d))
-    
+    #print("start")
 
-    t = 0.3
+    D = np.zeros(shape=(0,d))
+    i = 0
+
+    t = 0.1
 
     np.random.seed(seed=42)
 
@@ -46,12 +50,17 @@ if __name__ == "__main__":
         if random.random() < t:
             sample = np.fromstring(line, sep=" ")
             D = np.vstack( (D, sample) )
+            i+=1
+        #print(i)
 
 
 
     # Coresets via Adaptive Sampling
     B = []
     D_prime = D
+    n = len(D)
+    distances_to_centers = {}
+
     
     while len(D_prime) > 0:
         numSamples = min(len(D_prime), 10 * d * k * np.log(1/epsilon))
@@ -68,11 +77,11 @@ if __name__ == "__main__":
         distances_tuple = [t for t in distances_tuple if t[0] != 0.0]
         # Remove nearest points
         indices_to_remove = uniq(map(lambda e: e[1], distances_tuple))
-        if verbose:
-            print '--------------------'
-            print 'Number of samples = '+ str(numSamples)
-            print 'Previous len of D_prime = '+ str(len(D_prime))
-            print 'New len of D_prime = '+ str(len(D_prime)/2)
+        # if verbose:
+        #     print '--------------------'
+        #     print 'Number of samples = '+ str(numSamples)
+        #     print 'Previous len of D_prime = '+ str(len(D_prime))
+        #     print 'New len of D_prime = '+ str(len(D_prime)/2)
 
 
         # D_prime = np.asarray([ D_prime[t] for t in indices_to_remove[:(len(D_prime)/2)] ])
@@ -85,24 +94,43 @@ if __name__ == "__main__":
 
     # Remove enventual duplicates
     B = np.unique(map(lambda a: str(list(a.flat)), B))
-
+    Db = np.zeros(shape=(1,len(B)))
     tree = KDTree(B)
+    C = []
+
+    voronoi_cells = {k:[] for k in B}
+
+    buildVoronoi = True
+
+    for center in B:
+        sumDist = 0.0
+        for sample in D:
+            if buildVoronoi:
+                # nearest_center_index = tree.query(sample)[1]
+                # nearest_center = B[nearest_center_index]
+                # voronoi_cells[nearest_center].append(sample)
+                Db[nearest_center_index] += 1
+            sumDist += euclidean(sample, center)
+        buildVoronoi = False
+        distances_to_centers[center] = sumDist
 
 
-    
-    # if verbose:
-    #     print 'Now Voronoi partitioning'
-    
-    # vor = Voronoi(B)
-    # if verbose:
-    #     print 'Voronoi regions :'
-    #     print vor.regions
-    # if verbose:
-    #     print 'Adding D to vor :'
-    # vor.add_points(D)
+    cSize = 10*ceil((d*k*(log10(n)**2)*log10(1/delta)/epsilon**2))
+    sampled = 0
+    for sample in D:
+
+        # get nearest center id of sample
+        center_id = tree.query(sample)[1]
+        center = B[center_id]
+        gamma = gamma(sample, center, Db[center_id], distances_to_centers[center])
+        if gamma < random.random() and sampled < cSize:
+            C = np.vstack( (C, sample) )
 
 
-    emit(1, map(str, B))
+
+
+
+    emit(1, map(str, C))
         
 
 
